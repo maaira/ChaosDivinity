c5634d8f846865bb6e816bc7a59d8124e33489c7
maaira <38172242+maaira@users.noreply.github.com>
2019-03-21 02:16:04 -0300

    Inventory and Itens basic configurations

---
+++ ChaosDivinity/Item/Consumable.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChaosDivinity.Item
+{
+    class Consumable :  ItemGame
+    {
+        private int hp, mp;
+        public int HP { get => hp; set => hp = value; }
+        public int MP{ get => mp; set => mp = value; }
+
+        public Consumable(string name, string description, int draqui, int cash, int hp , int mp, string id) : base(name, description, draqui ,cash, id)
+        {
+            this.hp = hp;
+            this.mp = mp;
+        }
+
+
+    }
+}
--- ChaosDivinity/Item/ItemGame.cs
+++ ChaosDivinity/Item/ItemGame.cs
@@ -9,47 +9,34 @@ namespace ChaosDivinity.Item
 {
     class ItemGame
     {
+        
         /* ---------- Objetos da classe ------------- */
         private String name, description; //nome do item/equipamento/consumívle
-        private int def; //defesa que o equip dá
-        private int dmg; //dano
-        private bool equipped; //"retorna" se está equipado ou não
-        private string classGroup; //qual profissão poder usar o item
-        private string type; //arma, armadura, acessório, consumível, outros
-        private string category; //espada, adaga, cajado
-        //transformar type e category para enum (ou em último caso um dicionário) <------
         private int marketValueDraquima; //valor no NPC
         private int marketValueCash ; // 
         private Image sprite; //Imagem
-
+        private string id;
         /*  ---------- Objetos para instaciar ----------- */
         public string Name { get => name; set => name = value; }
         public string Description { get => description; }
-        public int Def { get => def;  }
-        public int Dmg { get => dmg;  }
-        public bool Equipped { get => equipped; set => equipped = value;}
-        public string ClassGroup { get => classGroup;  }
-        public string Typo { get => type;}
-        public string Category { get => category;  }
         public int MarketValueD { get => marketValueDraquima; }
         public int MarketValueC { get => marketValueCash; }
         public Image Sprite { get => sprite; set => sprite = value; }
-        
+        public string ID { get => id; }
 
 
-        public ItemGame(string name, string description, int def, int dmg, bool equipped, string classGroup , string typo , string category , int draqui, int cash )
+
+        public ItemGame(string name, string description, int draqui, int cash, string id )
         {
             this.name = name;
             this.description = description;
-            this.def = def;
-            this.dmg = dmg;
-            this.equipped = equipped;
-            this.classGroup = classGroup;
-            this.type = typo;
-            this.category = category;
             this.marketValueCash = cash;
             this.marketValueDraquima = draqui;
-
+            this.id = id;
         }
+
+        
+
+
     }
 }
