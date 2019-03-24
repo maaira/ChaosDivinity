132a8d07542bbf07ff52e42f37d94dec061d5417
João Victor Sledz de Bulhões <victorbulhoes9@hotmail.com>
2019-03-23 21:30:11 -0300

    Novos métodos para o Char, criação da Rogue e Skill

--- ChaosDivinity/Char/Character.cs
+++ ChaosDivinity/Char/Character.cs
@@ -8,16 +8,103 @@ namespace RPGTESTE.Char
 {
     abstract class Character
     {
-        public double skilllenght;
-        public double atacklenght;
+        //public double skilllenght;
+        //public double atacklenght;
         private string nameChar;
         private int hp_atual, hp_total;
         private int mp_atual, mp_total;
         private int xp_atual, xp_total, lvl;
         private int base_dmg, base_def;
         private string nameClass;
-        //private List<Skill> skills;
-        //private List<Itens> inventario = new List<Itens>();
+        private List<Skill> skills;
+        private List<Itens> inventario = new List<Itens>();
+
+
+        public string Nome { get => nome; set => nome = value; }
+        public int Hp_atual { get => hp_atual; set => hp_atual = value; }
+        public int Hp_total { get => hp_total; set => hp_total = value; }
+        public int Mp_atual { get => mp_atual; set => mp_atual = value; }
+        public int Mp_total { get => mp_total; set => mp_total = value; }
+        public int Xp_atual { get => xp_atual; set => xp_atual = value; }
+        public int Xp_total { get => xp_total; set => xp_total = value; }
+        public int Lvl { get => lvl; set => lvl = value; }
+        public int Base_dmg { get => base_dmg; set => base_dmg = value; }
+        public int Base_def { get => base_def; set => base_def = value; }
+
+        //Verifica se o ganho de experiência fez o personagem passar de level
+        public bool IsLvUP() {
+            if (Xp_atual >= Xp_total) {
+                return true; //Passou de level
+            }
+            else {
+                return false; // Não passou de level
+            }
+        }
+
+        // Virtual para poder realizar override
+        public virtual void LvUp() {
+            Lvl++;
+            xp_atual = xp_atual - xp_total;
+            Xp_total *= 2;
+            hp_atual = Hp_total;
+            mp_atual = Mp_total;
+            if (IsLvUP() == true) {
+                LvUp(); //Função recursiva para caso ganhe experiência suficiente para passar mais de uma vez de level
+            }
+        }
+
+        //Realiza o aumento de experiência
+        public void Gain_xp(int xp_gain) {
+            Xp_atual += xp_gain;
+            if (IsLvUP() == true) {
+                LvUp();
+            }
+        }
+
+        //Verifica se tem mana o suficiente para usar a habilidade
+        public bool IsManaAvaliable(int custo_de_mana) {
+            if (Mp_atual >= custo_de_mana) {
+                return true; // Pode usar a habilidade
+            }
+            else {
+                return false; //Não pode usar a habilidade
+            }
+        }
+
+        //Verifica se o personagem está vivo
+        public bool IsAlive() {
+            if (Hp_atual > 0) {
+                return true; // Está vivo
+            }
+            else {
+                return false; // Morreu
+            }
+        }
+
+        //Calcula o dano com base nos items equipados
+        public int Atk_base() {
+            int damage_add = 0;
+            foreach (Itens i in Inventario) {
+                if (i != null) {
+                    if (i.Equipado == true) {
+                        damage_add += i.Dmg;
+                    }
+                }
+            }
+            return Base_dmg + damage_add;
+        }
+
+        //Verifica se o golpe passa da defesa e modifica o HP
+        public bool Take_dmg(int dmg) {
+            if (dmg > Base_def) {
+                Hp_atual -= dmg;
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+
 
     }
 }
---
+++ ChaosDivinity/Char/Rogue.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace RPGTESTE.Char
+{
+    public class Rogue : Character
+    {
+
+        //Construtor genérico
+        public Rogue(String nome)
+        {
+
+            //Inicializa atributos do personagem
+
+            this.Nome = nome;
+            Lvl = 1; Xp_atual = 0; Xp_total = 0;
+            Hp_total = 0; Hp_atual = Hp_total;
+            Mp_total = 0; Mp_atual = Mp_total;
+            Base_def = 0; Base_dmg = 0;
+            Nome_classe = "Rogue";
+            
+            //Cria e inicializa as skills da classe do personagem
+            Skills = new List<Skill>();
+            //(String skill_name, custo_mp, custo_hp, lv_requerido, hp_up, mp_up, base_dmg_up, base_def_up, skill_dmg)
+            Skills.Add(new Skill("NomeDaHabilidade", 20, 0, 1, 0, 0, 0, 0, 200));
+
+        }
+
+        public override void LvUp()
+        {
+            Lvl++;
+            Xp_atual = Xp_atual - Xp_total;
+            Xp_total *= 2;
+            Hp_total += 0;
+            Mp_total += 0;
+            Base_def += 0;
+            Base_dmg += 0;
+            Hp_atual = Hp_total;
+            Mp_atual = Mp_total;
+            if (IsLvUP() == true)
+            {
+                LvUp();
+            }
+        }
+    }
+}
