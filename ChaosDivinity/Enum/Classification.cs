a13f24b233886a06e8add74e0d37f670c00c31fb
caiqueaugustocm <44869122+caiqueaugustocm@users.noreply.github.com>
2019-06-02 08:35:55 -0300

    Parte quest e bando de dados de itens feitas
    Eu coloquei algumas questes e os itens principais do jogo, só falta colocar o PATCH nos itens!

--- ChaosDivinity/Enum/Classification.cs
+++ ChaosDivinity/Enum/Classification.cs
@@ -4,7 +4,7 @@ namespace ChaosDivinity.Interface
     public class Classification
     {
         public enum Perso { Warrior = 0, Rogue = 1, Mage = 2, Children = 3 };
-        public enum TypeOf { Weapon = 0, Acessorio = 1, Colection = 2, Armo = 3 };
+        public enum TypeOf { Weapon = 0, Acessorio = 1, Colection = 2, Armor = 3 };
 
     }
 }
---
+++ ChaosDivinity/NPC/ListItemShop.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChaosDivinity.Inventory;
+using ChaosDivinity.Item;
+
+namespace ChaosDivinity.NPC
+{
+    class ListItemShop
+    {
+        static private Dictionary<int, InventorySlot> PotsShop;
+        
+
+
+
+        static private Dictionary<int, InventorySlot> ArmosShop;
+
+
+
+
+
+    }
+}
