8c4eeed836e9abdc0b342ccad85abc1d8f6a75d0
maaira <38172242+maaira@users.noreply.github.com>
2019-06-03 11:39:21 -0300

    Minor Changes

--- ChaosDivinity/Item/Item.cs
+++ ChaosDivinity/Item/Item.cs
@@ -1,10 +1,4 @@
 ﻿using ChaosDivinity.Physics;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 
 namespace ChaosDivinity.Item
 {
@@ -16,7 +10,7 @@ namespace ChaosDivinity.Item
         public int MarketValue { get; set; }
         public int SellValue { get; set; }
         public int Level { get; set; }
-        //public string ID { get; set; }
+        public string ID { get; set; }
         public string Path { get; set; }
 
     }
