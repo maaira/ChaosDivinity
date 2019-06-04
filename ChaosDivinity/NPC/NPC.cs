01089f252becc2f4a171286409b5f6ce6e326ecc
caiqueaugustocm <44869122+caiqueaugustocm@users.noreply.github.com>
2019-06-04 11:19:38 -0300

    Tirei algumas linhas

--- ChaosDivinity/NPC/NPC.cs
+++ ChaosDivinity/NPC/NPC.cs
@@ -1,9 +1,4 @@
 ﻿using ChaosDivinity.Physics;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ChaosDivinity.NPCNamespace
 {
